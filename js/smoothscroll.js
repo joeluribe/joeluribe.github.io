98825e8849bae837cd2b3cd70b120ea6209a49dd
joeluribe <joeluribe9@gmail.com>
2014-08-11 08:12:08 -0500

    functionality
    added modal and other functionality

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -35,3 +35,22 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
      });
 
 });
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50}, 400, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
